346f75da9d16dc40e6471b1a1fcb198e0cf4f0d9
pyk <2213646+pyk@users.noreply.github.com>
2025-05-21 06:37:19 +0700

    test: update

--- test/Kernel.t.sol
+++ test/Kernel.t.sol
@@ -2,9 +2,18 @@
 pragma solidity >=0.8.0;
 
 import { Test } from "forge-std/Test.sol";
+import { EntryPointDeployer } from "../lib/entrypoint-deployer/src/Deployer.sol";
 
-contract KernelTest is Test {
-    function setUp() external { }
+import { KernelDeployer } from "../src/Deployer.sol";
 
-    function testCreateAccount() external { }
+contract KernelTest is Test, EntryPointDeployer, KernelDeployer {
+    function setUp() external {
+        deployEntrypoint();
+        deployKernel();
+    }
+
+    function testCreateAccount() external {
+        // data
+        bytes memory data = "";
+    }
 }
