0496f0654d9e0742a4e01f603888a10be5208bb1
pyk <2213646+pyk@users.noreply.github.com>
2025-05-20 20:44:34 +0700

    chore: add KernelTest

--- test/Kernel.t.sol
+++ test/Kernel.t.sol
@@ -1,2 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
+
+import { Test } from "forge-std/Test.sol";
+
+contract KernelTest is Test {
+    function setUp() external { }
+
+    function testCreateAccount() external { }
+}
